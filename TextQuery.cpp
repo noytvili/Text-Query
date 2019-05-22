abc5f66ad869cd253184f2f61c68f41680955ae1
noytvili <33618257+noytvili@users.noreply.github.com>
2019-05-22 22:34:49 +0300

    Update TextQuery.cpp

--- TextQuery.cpp
+++ TextQuery.cpp
@@ -1,3 +1,6 @@
+/* Doriya Spielman 313466625 */
+/* Noy Tvili 308426790 */
+
 #include "TextQuery.h"
 #include <sstream>
 #include <string>
@@ -19,11 +22,13 @@ TextQuery::TextQuery(ifstream &is): file(new vector<string>)
 		int n = file->size() - 1;     // the current line number
 //////////////////////////////////////////////////////////////////////////
 		 istringstream line(text);     // separate the line into words
-		 string word;               
+		 string word;     
+
 		 while (line >> word) {        // for each word in that line
 		    auto start_words = sregex_iterator(word.begin(), word.end(), words_regex);
 			smatch match = *start_words;
-			word = match.str();
+			word = match.str(); 
+        //	std::cout << "bbbbbbbb" << std::endl;
 //////////////////////////////////////////////////////////////////////////	
             // if word isn't already in wm, subscripting adds a new entry
             auto &lines = wm[word]; // lines is a shared_ptr 
